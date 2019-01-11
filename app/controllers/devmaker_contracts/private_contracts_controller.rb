6fa7c87a50900c8697630a84ef94f8ceea42a51d
Rudiney Franceschi <rudi.atp@gmail.com>
2019-01-11 16:41:17 -0200

    fix the routes and complete the CRUD operation

--- app/controllers/devmaker_contracts/private_contracts_controller.rb
+++ app/controllers/devmaker_contracts/private_contracts_controller.rb
@@ -1,5 +1,7 @@
 module DevmakerContracts
   class PrivateContractsController < DevmakerContracts.private_controller
+    before_action :set_contract, only: [:show, :edit, :update, :destroy]
+
     def index
       @contracts = Contract.search_for(params[:q]).order(:id)
     end
@@ -32,13 +34,17 @@ module DevmakerContracts
     def destroy
       @contract.destroy
       flash[:success] = "Contrato removido."
-      redirect_to contract_path
+      redirect_to contracts_path
     end
 
     private
 
     def form_params
       params.require(:contract).permit([:name, :slug, :content_html])
+    end
+
+    def set_contract
+      @contract = Contract.find(params[:id])
     end
   end
 end
